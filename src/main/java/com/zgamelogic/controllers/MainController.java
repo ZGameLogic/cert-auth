e43d4cc13fecc91c05429d22dbb446d9bc2e151d
Ben Shabowski <benshabowski@gmail.com>
2025-03-23 16:14:50 -0500

    init

--- src/main/java/com/zgamelogic/controllers/MainController.java
+++ src/main/java/com/zgamelogic/controllers/MainController.java
@@ -2,10 +2,16 @@ package com.zgamelogic.controllers;
 
 import com.zgamelogic.discord.auth.data.database.authData.AuthDataRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @AllArgsConstructor
 public class MainController {
     private final AuthDataRepository authDataRepository;
+
+    @GetMapping("health")
+    public String health() {
+        return "Healthy";
+    }
 }
